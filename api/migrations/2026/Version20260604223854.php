9b1bc5196f4bbdefe5430b23aff6314972d3cba6
Sergio Salcedo <123974350+sergio-salcedo-dev@users.noreply.github.com>
2026-06-05 00:46:07 +0200

    Update Version20260604223854.php

--- api/migrations/2026/Version20260604223854.php
+++ api/migrations/2026/Version20260604223854.php
@@ -7,9 +7,6 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20260604223854 extends AbstractMigration
 {
     public function getDescription(): string
